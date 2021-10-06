955696b888950f61f941d2d11160a2f68ee7c63b
Đỗ Tiến Dũng <dungdotien14@gmail.com>
2021-10-06 23:41:53 +0700

    Initial commit

---
+++ app/src/main/java/com/example/message/activities/SignInActivity.java
@@ -0,0 +1,23 @@
+package com.example.message.activities;
+
+import android.content.Intent;
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import com.example.message.databinding.ActivitySignInBinding;
+
+public class SignInActivity extends AppCompatActivity {
+    private ActivitySignInBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivitySignInBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        setListeners();
+    }
+
+    private void setListeners() {
+        binding.textCreateNewAccount.setOnClickListener(v -> startActivity(
+                new Intent(getApplicationContext(), SignUpActivity.class)));
+    }
+}
---
+++ app/src/main/java/com/example/message/activities/SignUpActivity.java
@@ -0,0 +1,21 @@
+package com.example.message.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import com.example.message.databinding.ActivitySignUpBinding;
+
+public class SignUpActivity extends AppCompatActivity {
+    private ActivitySignUpBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivitySignUpBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        setListeners();
+    }
+
+    private void setListeners() {
+        binding.textSignIn.setOnClickListener(v -> onBackPressed());
+    }
+}
