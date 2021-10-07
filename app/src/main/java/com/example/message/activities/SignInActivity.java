fb10024ab47524674a6a6097d3ad5323200aed49
Đỗ Tiến Dũng <dungdotien14@gmail.com>
2021-10-07 19:22:42 +0700

    Set up Firebase Cloud Messaging & Firestore Database

--- app/src/main/java/com/example/message/activities/SignInActivity.java
+++ app/src/main/java/com/example/message/activities/SignInActivity.java
@@ -1,9 +1,13 @@
 package com.example.message.activities;
 
 import android.content.Intent;
+import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import com.example.message.databinding.ActivitySignInBinding;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
 
 public class SignInActivity extends AppCompatActivity {
     private ActivitySignInBinding binding;
@@ -19,5 +23,17 @@ public class SignInActivity extends AppCompatActivity {
     private void setListeners() {
         binding.textCreateNewAccount.setOnClickListener(v -> startActivity(
                 new Intent(getApplicationContext(), SignUpActivity.class)));
+        binding.buttonSignIn.setOnClickListener(v -> addDataToFireStore());
+    }
+
+    private void addDataToFireStore() {
+        FirebaseFirestore database = FirebaseFirestore.getInstance();
+        HashMap<String, Object> data = new HashMap<>();
+        data.put("first_name", "Dung");
+        data.put("last_name", "Do Tien");
+        database.collection("users")
+                .add(data)
+                .addOnSuccessListener(documentReference -> Toast.makeText(getApplicationContext(), "Data Inserted", Toast.LENGTH_SHORT).show())
+                .addOnFailureListener(exception -> Toast.makeText(getApplicationContext(), exception.getMessage(), Toast.LENGTH_SHORT).show());
     }
 }
