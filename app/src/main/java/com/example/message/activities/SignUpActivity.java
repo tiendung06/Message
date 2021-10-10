194667819ef132db40a3079a41ea29858c728aaa
Đỗ Tiến Dũng <dungdotien14@gmail.com>
2021-10-10 10:43:24 +0700

    Add select image profile

--- app/src/main/java/com/example/message/activities/SignUpActivity.java
+++ app/src/main/java/com/example/message/activities/SignUpActivity.java
@@ -1,9 +1,16 @@
 package com.example.message.activities;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.provider.MediaStore;
+import android.util.Base64;
 import android.util.Patterns;
 import android.view.View;
 import android.widget.Toast;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import com.example.message.databinding.ActivitySignUpBinding;
@@ -11,12 +18,16 @@ import com.example.message.utilities.Constants;
 import com.example.message.utilities.PreferenceManager;
 import com.google.firebase.firestore.FirebaseFirestore;
 
+import java.io.ByteArrayOutputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
 import java.util.HashMap;
 import java.util.Objects;
 
 
 public class SignUpActivity extends AppCompatActivity {
     private ActivitySignUpBinding binding;
+    private String encodeImage;
     private PreferenceManager preferenceManager;
 
     @Override
@@ -35,6 +46,11 @@ public class SignUpActivity extends AppCompatActivity {
                 signUp();
             }
         });
+        binding.layoutImage.setOnClickListener(v -> {
+            Intent intent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+            intent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
+            pickImage.launch(intent);
+        });
     }
 
     private void showToast(String message) {
@@ -48,6 +64,7 @@ public class SignUpActivity extends AppCompatActivity {
         user.put(Constants.KEY_NAME, Objects.requireNonNull(binding.inputName.getText()).toString());
         user.put(Constants.KEY_EMAIL, Objects.requireNonNull(binding.inputEmail.getText()).toString());
         user.put(Constants.KEY_PASSWORD, Objects.requireNonNull(binding.inputPassword.getText()).toString());
+        user.put(Constants.KEY_IMAGE, encodeImage);
         database.collection(Constants.KEY_COLLECTION_USERS)
                 .add(user)
                 .addOnSuccessListener(documentReference -> {
@@ -65,7 +82,41 @@ public class SignUpActivity extends AppCompatActivity {
                 });
     }
 
+    private String encodeImage(Bitmap bitmap) {
+        int previewWidth = 150;
+        int previewHeight = bitmap.getHeight() * previewWidth / bitmap.getWidth();
+        Bitmap previewBitMap = Bitmap.createScaledBitmap(bitmap, previewWidth, previewHeight, false);
+        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+        previewBitMap.compress(Bitmap.CompressFormat.JPEG, 50, byteArrayOutputStream);
+        byte[] bytes = byteArrayOutputStream.toByteArray();
+        return Base64.encodeToString(bytes, Base64.DEFAULT);
+    }
+
+    private final ActivityResultLauncher<Intent> pickImage = registerForActivityResult(
+            new ActivityResultContracts.StartActivityForResult(),
+            result -> {
+                if (result.getResultCode() == RESULT_OK) {
+                    if (result.getData() != null) {
+                        Uri imageUri = result.getData().getData();
+                        try {
+                            InputStream inputStream = getContentResolver().openInputStream(imageUri);
+                            Bitmap bitmap = BitmapFactory.decodeStream(inputStream);
+                            binding.imageProfile.setImageBitmap(bitmap);
+                            binding.textAddImage.setVisibility(View.GONE);
+                            encodeImage = encodeImage(bitmap);
+                        } catch (FileNotFoundException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }
+            }
+    );
+
     private Boolean isValidSignUpDetails() {
+        if (encodeImage == null) {
+            showToast("Select profile image");
+            return false;
+        }
         if (Objects.requireNonNull(binding.inputName.getText()).toString().trim().isEmpty()) {
             showToast("Nhap ten");
             return false;
