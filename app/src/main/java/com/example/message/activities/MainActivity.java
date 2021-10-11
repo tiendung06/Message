c00c5583a8f35f2fb9c9c400e8ac038a6f41e431
Đỗ Tiến Dũng <dungdotien14@gmail.com>
2021-10-11 20:38:45 +0700

    Display User List From Firestore Database

--- app/src/main/java/com/example/message/activities/MainActivity.java
+++ app/src/main/java/com/example/message/activities/MainActivity.java
@@ -34,6 +34,7 @@ public class MainActivity extends AppCompatActivity {
 
     private void setListeners() {
         binding.imageSignOut.setOnClickListener(v -> signOut());
+        binding.fabNewChat.setOnClickListener(v -> startActivity(new Intent(getApplicationContext(), UsersActivity.class)));
     }
 
     private void loadUserDetails() {
@@ -57,7 +58,6 @@ public class MainActivity extends AppCompatActivity {
                 preferenceManager.getString(Constants.KEY_USER_ID)
         );
         documentReference.update(Constants.KEY_FCM_TOKEN, token)
-                .addOnSuccessListener(unused -> showToast("Token update successfully"))
                 .addOnFailureListener(e -> showToast("Unable to update token"));
     }
 
---
+++ app/src/main/java/com/example/message/models/User.java
@@ -0,0 +1,10 @@
+package com.example.message.models;
+
+import java.io.Serializable;
+
+public class User implements Serializable {
+    public String name;
+    public String image;
+    public String email;
+    public String token;
+}
