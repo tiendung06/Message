328ec472f55d9aa04c0678197d085918fdca54eb
Đỗ Tiến Dũng <dungdotien14@gmail.com>
2021-10-12 11:32:35 +0700

    Add Chat Screen Layout Design

---
+++ app/src/main/java/com/example/message/activities/ChatActivity.java
@@ -0,0 +1,14 @@
+package com.example.message.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import com.example.message.R;
+
+public class ChatActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_chat);
+    }
+}
