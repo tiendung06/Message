3b399f2516b6be17ef3abd8929257d6bacdba2e5
Đỗ Tiến Dũng <dungdotien14@gmail.com>
2021-10-14 13:25:33 +0700

    Add Realtime Chat Implementation

--- app/src/main/java/com/example/message/models/ChatMessage.java
+++ app/src/main/java/com/example/message/models/ChatMessage.java
@@ -1,8 +1,11 @@
 package com.example.message.models;
 
+import java.util.Date;
+
 public class ChatMessage {
     public String senderId;
     public String receiverId;
     public String message;
     public String dateTime;
+    public Date dateObject;
 }
