e4e352b181a3ec04b33c23893ea53799a14a18c3
Đỗ Tiến Dũng <dungdotien14@gmail.com>
2021-10-25 21:55:38 +0700

    Change button sign out

--- app/src/main/java/com/example/message/activities/MainActivity.java
+++ app/src/main/java/com/example/message/activities/MainActivity.java
@@ -50,7 +50,6 @@ public class MainActivity extends BaseActivity implements ConversionListener {
     }
 
     private void setListeners() {
-        binding.imageSignOut.setOnClickListener(v -> signOut());
         binding.fabNewChat.setOnClickListener(v -> startActivity(new Intent(getApplicationContext(), UsersActivity.class)));
         binding.imageProfile.setOnClickListener(v -> startActivity(new Intent(getApplicationContext(), UpdateProfileActivity.class)));
     }
@@ -130,21 +129,6 @@ public class MainActivity extends BaseActivity implements ConversionListener {
         );
         documentReference.update(Constants.KEY_FCM_TOKEN, token)
                 .addOnFailureListener(e -> showToast("Unable to update token"));
-    }
-
-    private void signOut() {
-        showToast("Signing out...");
-        FirebaseFirestore database = FirebaseFirestore.getInstance();
-        DocumentReference documentReference = database.collection(Constants.KEY_COLLECTION_USERS).document(
-                preferenceManager.getString(Constants.KEY_USER_ID));
-        HashMap<String, Object> updates = new HashMap<>();
-        updates.put(Constants.KEY_FCM_TOKEN, FieldValue.delete());
-        documentReference.update(updates).addOnSuccessListener(unused -> {
-                    preferenceManager.clear();
-                    startActivity(new Intent(getApplicationContext(), SignInActivity.class));
-                    finish();
-                })
-                .addOnFailureListener(e -> showToast("Unable to sign out"));
     }
 
     @Override
--- app/src/main/java/com/example/message/activities/UpdateProfileActivity.java
+++ app/src/main/java/com/example/message/activities/UpdateProfileActivity.java
@@ -1,13 +1,20 @@
 package com.example.message.activities;
 
+import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.util.Base64;
+import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
 import com.example.message.databinding.ActivityUpdateProfileBinding;
 import com.example.message.utilities.Constants;
 import com.example.message.utilities.PreferenceManager;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FieldValue;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
 
 public class UpdateProfileActivity extends AppCompatActivity {
     private ActivityUpdateProfileBinding binding;
@@ -26,6 +33,7 @@ public class UpdateProfileActivity extends AppCompatActivity {
 
     private void setListener() {
         binding.imageBack.setOnClickListener(v -> onBackPressed());
+        binding.imageSignOut.setOnClickListener(v -> signOut());
     }
 
     private void loadUserDetails() {
@@ -33,5 +41,23 @@ public class UpdateProfileActivity extends AppCompatActivity {
         byte[] bytes = Base64.decode(preferenceManager.getString(Constants.KEY_IMAGE), Base64.DEFAULT);
         Bitmap bitmap = BitmapFactory.decodeByteArray(bytes, 0, bytes.length);
         binding.imageProfile.setImageBitmap(bitmap);
+    }
+
+    private void showToast(String message) {
+        Toast.makeText(getApplicationContext(), message, Toast.LENGTH_SHORT).show();
+    }
+
+    private void signOut() {
+        FirebaseFirestore database = FirebaseFirestore.getInstance();
+        DocumentReference documentReference = database.collection(Constants.KEY_COLLECTION_USERS).document(
+                preferenceManager.getString(Constants.KEY_USER_ID));
+        HashMap<String, Object> updates = new HashMap<>();
+        updates.put(Constants.KEY_FCM_TOKEN, FieldValue.delete());
+        documentReference.update(updates).addOnSuccessListener(unused -> {
+                    preferenceManager.clear();
+                    startActivity(new Intent(getApplicationContext(), SignInActivity.class));
+                    finish();
+                })
+                .addOnFailureListener(e -> showToast("Unable to sign out"));
     }
 }
